06f5cc852fe7a2628bb70f3236ce818d516e6249
zmeditation <senior.zsoftware@gmail.com>
2023-11-24 21:38:43 +0900

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,68 +3,66 @@ import axios from "axios";
 import { APIS_BASE_URL } from "../constants";
 
 export const getSignInAPI = async (payload: any) => {
-    return await axios.post(`${APIS_BASE_URL}/api/signIn`, payload);
+  return await axios.post(`${APIS_BASE_URL}/api/signIn`, payload);
 };
 
 export const checkTokenAPI = async (accessToken: any) => {
-    return await axios({
-        method: "get",
-        url: `${APIS_BASE_URL}/api/check-token`,
-        headers: accessToken
-            ? { cookie: `dynaswapToken=${accessToken}` }
-            : undefined,
-    });
+  return await axios({
+    method: "get",
+    url: `${APIS_BASE_URL}/api/check-token`,
+    headers: accessToken
+      ? { cookie: `dynaswapToken=${accessToken}` }
+      : undefined,
+  });
 };
 
 export const addContractAPI = async (payload: any) => {
-    return await axios.post(`${APIS_BASE_URL}/api/add-contract`, payload);
+  return await axios.post(`${APIS_BASE_URL}/api/add-contract`, payload);
 };
 
 export const getContractsAPI = async (page = 0) => {
-    return await axios.get(`${APIS_BASE_URL}/api/get-contracts`, {
-        params: { page },
-    });
+  return await axios.get(`${APIS_BASE_URL}/api/get-contracts`, {
+    params: { page },
+  });
 };
 
 export const getContractsByChainIdAPI = async (
-    chainId: any,
-    account: any,
-    cursor = null
+  chainId: any,
+  account: any,
+  cursor = null
 ) => {
-    return await axios.get(`${APIS_BASE_URL}/api/get-contracts-by-chainId`, {
-        params: { chainId, account, cursor },
-    });
+  const res = await axios.get(
+    `https://onft-backend.vercel.app/api/get-tokens/account/${account}/chain/${chainId}`
+  );
+  return res;
 };
 
 export const getContractInfoByAddressAPI = async (
-    accessToken: any,
-    address: any
+  accessToken: any,
+  address: any
 ) => {
-    return await axios.get(
-        `${APIS_BASE_URL}/api/get-contract-info-by-address`,
-        {
-            params: {
-                address,
-            },
-            headers: accessToken
-                ? { cookie: `dynaswapToken=${accessToken}` }
-                : undefined,
-        }
-    );
+  return await axios.get(`${APIS_BASE_URL}/api/get-contract-info-by-address`, {
+    params: {
+      address,
+    },
+    headers: accessToken
+      ? { cookie: `dynaswapToken=${accessToken}` }
+      : undefined,
+  });
 };
 
 export const updateContractInfoByAddressAPI = async (payload: any) => {
-    return await axios.post(
-        `${APIS_BASE_URL}/api/update-contract-by-address`,
-        payload
-    );
+  return await axios.post(
+    `${APIS_BASE_URL}/api/update-contract-by-address`,
+    payload
+  );
 };
 
 export const deleteContractInfoByAddressAPI = async (address: any) => {
-    return await axios.delete(
-        `${APIS_BASE_URL}/api/remove-contract-info-by-address`,
-        {
-            params: { address },
-        }
-    );
+  return await axios.delete(
+    `${APIS_BASE_URL}/api/remove-contract-info-by-address`,
+    {
+      params: { address },
+    }
+  );
 };
